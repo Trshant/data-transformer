166108aa0a0046446499348ac4dd78737bcd6eb8
trshant <trshant@gmail.com>
2019-07-31 06:16:32 +0200

    second commit

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 We all do data transformation, but the solution is unelegant and thoroughly not literate. By using pure functions in a functional programming paradigm, we speed up data transformations without compromising on elegance.  
 
 Do read through the uses and let me know via the issues or email me ( trshant at gmail dot com ) if something can be made better. You can email me if i did it right - i'd love to hear that!  
+
+# Documentation.
+
+only one function needs to be called post importing: `process`.  
+```python
+process(data, process_line_definition)
+```  
+data is an imput object.
+the process_line_definition is what we use to define what we need done. The format for it is:  
+
+```
+[ data_path , directive , function_called_on_data_object_after_going_on_data_path , further_process_line_directive ]
+```  
--- uses/reddit_search.py
+++ uses/reddit_search.py
@@ -10,10 +10,12 @@ def get_data(url):
     data = r.json()
     return data
 
-def get_data_make_url(part_of_url):
-    return get_data(part_of_url+'.json')
+def process_comments(part_of_url):
+    global reddit_comment_process
+    data = get_data(part_of_url+'.json')
+    return process( data , reddit_comment_process )
 
-def dofunc(data):
+def search_print(data):
     try:
         urlurl = "https://reddit.com" + data["data"]["permalink"]
         body = data["data"]["body"]
@@ -21,24 +23,28 @@ def dofunc(data):
             print(urlurl)
     except KeyError:
         pass
+    return data
 
-process_line_definition = [
-    "data/children" ,
-    "loop" ,
-    None ,
+reddit_search_process = [
+    "data/children" , # go here first
+    "loop" , # loop through the array
+    None ,  # Dont do anything here; allow the succeeding process line to work through each item in the loop
     [
-        "data/url" ,
-        "process",
-        get_data_make_url ,
-        [
-            "1/data/children" ,
-            "loop",
-            dofunc,
-            []
-        ]
+        "data/url" , # go here
+        "process" , # process this
+        process_comments ,  # process the url to process comments in post
+        []
     ]
+]
+
+reddit_comment_process = [
+    "1/data/children",  # go down this data path
+    "loop",  # you know what happens here ;-)
+    # see the defninition of this function to see what this does - to search if the comment has the word 'news' in it. if it does, print the url out.
+    search_print,
+    []  # shtap!
 ]
 
 data = get_data(
     r'http://www.reddit.com/r/india/search/.json?q=Weekly%20Coders%2C%20Hackers%20%26%20All%20Tech%20related%20thread%20news&restrict_sr=1')
-process(data, process_line_definition)
+process(data, reddit_search_process)
